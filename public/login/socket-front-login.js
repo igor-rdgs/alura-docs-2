03166e6d6189e8ceba34f23c8adadc8309d9bd33
Igor Rodrigues <igor.aguiar1@live.com>
2025-12-05 19:52:09 -0300

    feat: adicionando JWT

--- public/login/socket-front-login.js
+++ public/login/socket-front-login.js
@@ -8,8 +8,8 @@ socket.on("autenticacao_sucesso", () => {
 	alert("Usuário autenticado com sucesso!");
 	window.location.href = "/";
 });
+
 socket.on("autenticacao_erro", () => alert("Erro na autenticação."));
 socket.on("usuario_nao_encontrado", () => alert("Usuário não encontrado."));
-
 
 export { emitirAutenticarUsuario };
