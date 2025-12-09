8cf3da5e3341fd637b8c42385685c4fef656e5c4
Igor Rodrigues <igor.aguiar1@live.com>
2025-12-09 15:18:47 -0300

    feat: atualizando página de documento com usuários conectados

--- public/documento/documento.js
+++ public/documento/documento.js
@@ -10,13 +10,23 @@ const nomeDocumento = parametros.get("nome");
 const textoEditor = document.getElementById("editor-texto");
 const tituloDocumento = document.getElementById("titulo-documento");
 const botaoExcluir = document.getElementById("excluir-documento");
+const listaUsuariosConectados = document.getElementById("usuarios-conectados");
 
 tituloDocumento.textContent = nomeDocumento || "Documento sem título";
 
 function tratarAutorizacaoSucesso(payloadToken) {
   selecionarDocumento({ nomeDocumento, nomeUsuario: payloadToken.nomeUsuario });
-}
+};
 
+function atualizarInterfaceUsuarios(usuariosNoDocumento) {
+  listaUsuariosConectados.innerHTML = "";
+
+  usuariosNoDocumento.forEach((usuario) => {
+    listaUsuariosConectados.innerHTML += `
+      <li class="list-group-item">${usuario}</li>
+    `;
+  });
+};
 
 textoEditor.addEventListener("keyup", () => {
   emitirTextoEditor({
@@ -27,7 +37,7 @@ textoEditor.addEventListener("keyup", () => {
 
 function atualizaTextoEditor(texto) {
   textoEditor.value = texto;
-}
+};
 
 botaoExcluir.addEventListener("click", () => {
   emitirExcluirDocumento(nomeDocumento);
@@ -37,7 +47,7 @@ function alertarERedirecionar(nome) {
   if (nome === nomeDocumento) {
     alert(`Documento ${nome} excluído!`);
     window.location.href = "/";
-  }
-}
+  };
+};
 
-export { atualizaTextoEditor, alertarERedirecionar, tratarAutorizacaoSucesso };
+export { atualizaTextoEditor, alertarERedirecionar, tratarAutorizacaoSucesso, atualizarInterfaceUsuarios };
