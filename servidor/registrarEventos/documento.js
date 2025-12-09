e6afa8e94c7d0ccc530b35150f89d132f6c6c963
Igor Rodrigues <igor.aguiar1@live.com>
2025-12-09 16:03:36 -0300

    feat: adicionando saída na lista local de conexões

--- servidor/registrarEventos/documento.js
+++ servidor/registrarEventos/documento.js
@@ -3,7 +3,7 @@ import {
   encontrarDocumento,
   excluirDocumento,
 } from "../db/documentosDb.js";
-import { adicionarConexao, obterUsuariosDocumento } from "../utils/conexoesDocumentos.js";
+import { adicionarConexao, obterUsuariosDocumento, removerConexao } from "../utils/conexoesDocumentos.js";
 
 function registrarEventosDocumento(socket, io) {
   socket.on(
@@ -40,7 +40,11 @@ function registrarEventosDocumento(socket, io) {
       });
 
       socket.on("disconnect", () => {
-        console.log(`Cliente ${socket.id} foi desconectado.`)
+        removerConexao(nomeDocumento, nomeUsuario);
+
+        const usuariosNoDocumento = obterUsuariosDocumento(nomeDocumento);
+
+        io.to(nomeDocumento).emit("usuarios_no_documento", usuariosNoDocumento);
       });
     }
   );
